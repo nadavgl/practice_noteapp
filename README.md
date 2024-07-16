547136cfc821dcb2a5dc58e10ce3bc48edd0f143
nadavgl <ngliksht@gmail.com>
2024-07-16 14:14:24 -0400

    added the ignore file

---
+++ README.md
@@ -0,0 +1,10 @@
+
+  # Title
+  ya
+
+  # Description
+  ya
+
+  # Installation
+   
+  
--- index.js
+++ index.js
@@ -1,15 +1,73 @@
-const showMainMenu = require('./lib/showMainMenu');
+const inquirer = require('inquirer');
+const fs = require('fs').promises
+const steps = []
 
-function init() {
-  // Show a welcome message
-  console.log(`
-  -----------
-  Welcome to the ToDo Wiz
-  -----------
-  `);
 
-  // Show the menu options
-  showMainMenu()
+const questions = [
+  {
+    name: 'title',
+    message: 'Please enter a title'
+  },
+  {
+    name:'description',
+    message:'please enter a description'
+  }
+];
+
+function askStartingQuestions() {
+  return inquirer.prompt(questions)
+}
+
+function askForIntallSteps(initialAnswersObj){
+  const addStep = () => {
+    return inquirer.prompt({
+      name: 'step',
+      type: 'list',
+      message: 'Please enter the step text'
+    }).then(stepAnswerObj => {
+      steps.push(stepAnswerObj.step)
+    })
+  }
+  inquirer.prompt({
+    name: 'step',
+    message: 'Please select an installation step option',
+    choices: ['Add and installation step', 'Finish installation steps']
+  }).then(choiceAnswer =>{
+    switch(choiceAnswer.choice){
+      case 'Add an installation step':
+        return addStep()
+          .then(askForIntallSteps);
+          default: 
+            return finalQuestions(initialAnswersObj, steps);
+    }
+
+  })
+}
+
+function finalQuestions(initialAnswers, steps){
+  //Final prompt and then end by generating a READme.md file
+  console.log('Final prompt')
+
+  const md = `
+  # Title
+  ${initialAnswers.title}
+
+  # Description
+  ${initialAnswers.description}
+
+  # Installation
+   ${steps.map(step => `${step}`).join('\n')}
+  `
+
+  return fs.writeFile('./README.md', md)
+}
+
+function init(){
+  askStartingQuestions()
+    .then(askForIntallSteps)
+    .then(() => {
+      console.log('Readme Generated')
+    })
 }
 
 init();
