631365df94ee9d1964f3608d1c8887a7086ebe1a
nadavgl <ngliksht@gmail.com>
2024-07-18 20:10:59 -0400

    started server basics

--- index.js
+++ index.js
@@ -1,73 +1,20 @@
-const inquirer = require('inquirer');
-const fs = require('fs').promises
-const steps = []
+const express = require('express')
+const cors = require('cors')
 
+const app = express();
 
-const questions = [
-  {
-    name: 'title',
-    message: 'Please enter a title'
-  },
-  {
-    name:'description',
-    message:'please enter a description'
-  }
-];
+// Base route - domain.com (root route) is represented by a '/' - this slash directly follows the domain (domain.com/)
 
-function askStartingQuestions() {
-  return inquirer.prompt(questions)
-}
+app.use(cors())
 
-function askForIntallSteps(initialAnswersObj){
-  const addStep = () => {
-    return inquirer.prompt({
-      name: 'step',
-      type: 'list',
-      message: 'Please enter the step text'
-    }).then(stepAnswerObj => {
-      steps.push(stepAnswerObj.step)
-    })
-  }
-  inquirer.prompt({
-    name: 'step',
-    message: 'Please select an installation step option',
-    choices: ['Add and installation step', 'Finish installation steps']
-  }).then(choiceAnswer =>{
-    switch(choiceAnswer.choice){
-      case 'Add an installation step':
-        return addStep()
-          .then(askForIntallSteps);
-          default: 
-            return finalQuestions(initialAnswersObj, steps);
-    }
+app.get('/', (requestObj, responseObj) => {
+  responseObj.send({
+    name: 'Nadav',
+    age: 2
+  });
 
-  })
-}
+});
 
-function finalQuestions(initialAnswers, steps){
-  //Final prompt and then end by generating a READme.md file
-  console.log('Final prompt')
-
-  const md = `
-  # Title
-  ${initialAnswers.title}
-
-  # Description
-  ${initialAnswers.description}
-
-  # Installation
-   ${steps.map(step => `${step}`).join('\n')}
-  `
-
-  return fs.writeFile('./README.md', md)
-}
-
-function init(){
-  askStartingQuestions()
-    .then(askForIntallSteps)
-    .then(() => {
-      console.log('Readme Generated')
-    })
-}
-
-init();
+app.listen(3333, () => {
+  console.log('Server started')
+})
